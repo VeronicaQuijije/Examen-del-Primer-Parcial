7e8361cab42589ccad0ba7a78d1043cf6cc77102
VeronicaQuijije <89811841+VeronicaQuijije@users.noreply.github.com>
2022-05-31 22:28:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// Aplicar  el metodo POST de un servicio que reciba un objeto con 3 arreglos y permita unirlos en un nuevo, para luego devolverlo
+//al usuario como parte de un objeto literal.
+function nombre();
+{
+    var nombre = ("Picapiedras");
+    boy ("Nombre","Apellido")
+    let ("Pedro","Picapiedras");
+}
+
+ function nombre(Simpsons);
+ {
+ var nombre = ("Homero" );
+ var apellido = ("Simpsons");
+ const require = require ("nombre" , "Apellido");
+}
+function nombre(Jetson);
+ {
+ var nombre = ("George" );
+ var apellido = ("Simpsons");
+ const require = require ("nombre" , "Apellido");
+}
+
+     
+       
+// Desarrollar el metodo Post de un servicio REST que reciba los datos de un usuario y aplicando un middware valide de manera previa que los datos cumplas
+
+()
